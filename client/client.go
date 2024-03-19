a5e9d5699934cc4f9495b8f1a84d25bad224238a
loomt <loomt_@outlook.com>
2024-03-19 15:00:43 +0800

    feat:change docker func to ansible

--- client/client.go
+++ client/client.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"context"
+	"fmt"
+	"path"
 	"sync"
 	"time"
 
@@ -9,6 +11,7 @@ import (
 	"github.com/ipfs-cluster/ipfs-cluster/api/rest/client"
 	files "github.com/ipfs/boxo/files"
 	logging "github.com/ipfs/go-log/v2"
+	"github.com/loomts/ipfs-cluster-erasure-example/utils"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
@@ -18,35 +21,62 @@ type Client struct {
 
 var log = logging.Logger("EClient")
 var defaultTimeout = 3 * time.Minute
-var ECAddParams, DefaultAddParams api.AddParams
+var ECAddParams api.AddParams
+var DefaultAddParams api.AddParams
 
 func init() {
 	ECAddParams = api.DefaultAddParams()
 	ECAddParams.ReplicationFactorMin = 1
 	ECAddParams.ReplicationFactorMax = 1
 	ECAddParams.Erasure = true // automatically enable shard and raw-leaves
-	ECAddParams.DataShards = 4
-	ECAddParams.ParityShards = 2
+	ECAddParams.DataShards = 10
+	ECAddParams.ParityShards = 4
 
 	DefaultAddParams = api.DefaultAddParams()
 	DefaultAddParams.Shard = true
 	DefaultAddParams.RawLeaves = true
-	DefaultAddParams.ReplicationFactorMin = 3
-	DefaultAddParams.ReplicationFactorMax = 3
+	DefaultAddParams.ReplicationFactorMin = 4
+	DefaultAddParams.ReplicationFactorMax = 4
 }
 
-func NewClient() (Client, error) {
+func NewClient() (*Client, error) {
 	addr := "/ip4/127.0.0.1/tcp/9094"
 	maddr, err := ma.NewMultiaddr(addr)
 	if err != nil {
-		return Client{}, err
+		return &Client{}, err
 	}
 	cfg := client.Config{APIAddr: maddr}
 	c, err := client.NewLBClient(&client.Failover{}, []*client.Config{&cfg}, 1)
 	if err != nil {
-		return Client{}, err
+		return &Client{}, err
 	}
-	return Client{c}, nil
+	return &Client{c}, nil
+}
+
+func (c *Client) AddFile(f utils.ECFile) (api.Cid, error) {
+	defer f.Closer.Close()
+	params := DefaultAddParams
+	params.Name = f.Name
+	st := time.Now()
+	pin, err := c.Add(context.Background(), path.Join(f.Base, f.Name), params)
+	if err != nil {
+		return api.CidUndef, err
+	}
+	fmt.Printf("add %s(%v), size:%v, use %v\n", pin.Name, pin.Cid, f.Size, time.Since(st))
+	return pin.Cid, nil
+}
+
+func (c *Client) AddECFile(f utils.ECFile) (api.Cid, error) {
+	defer f.Closer.Close()
+	params := ECAddParams
+	params.Name = f.Name
+	st := time.Now()
+	pin, err := c.Add(context.Background(), path.Join(f.Base, f.Name), params)
+	if err != nil {
+		return api.CidUndef, err
+	}
+	fmt.Printf("add %s(%v), size:%v, use %v\n", pin.Name, pin.Cid, f.Size, time.Since(st))
+	return pin.Cid, nil
 }
 
 func (c *Client) AddMultiFile(ctx context.Context, multiFileR *files.MultiFileReader, params api.AddParams) (api.AddedOutput, error) {
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"math/rand"
 	"os/exec"
 	"path"
 	"time"
@@ -12,139 +11,38 @@ import (
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/loomts/ipfs-cluster-erasure-example/client"
 	"github.com/loomts/ipfs-cluster-erasure-example/utils"
-
-	"github.com/docker/docker/api/types/container"
-	dockercli "github.com/docker/docker/client"
 )
 
 var log = logging.Logger("cluster")
 
+// before test, ensure cluster is set up and running
 func main() {
-	// TestAllAndFaultToler()
-
-	// fmt.Println("ecadd_diff")
-	// TestAddECFile_DiffSize()
-	// fmt.Println("ecadd_same")
-	// TestAddECFile_LargeSameSize()
-
-	// fmt.Println("add_diff")
-	// TestAddFile_DiffSize()
-
-	// fmt.Println("add_same")
-	// TestAddFile_LargeSameSize()
-
 	// fmt.Println("get_diff")
-	// TestGetFile_DiffSize()
+	// TestGetFileDiffSize()
 	// fmt.Println("get_same")
-	// TestGetFile_LargeSameSize()
-	// fmt.Println("ecget_diff")
-	// TestECGetFile_DiffSize()
+	// TestGetFileLargeSameSize()
+
+	fmt.Println("ecget_diff")
+	TestECGetFileDiffSize()
 	// fmt.Println("ecget_same")
-	// TestECGetFile_LargeSameSize()
+	// TestECGetFileLargeSameSize()
 
 	//fmt.Println("ecget_recovery")
 	//TestECRecovery()
-	utils.Draw()
+	// utils.Draw()
 }
 
-func start() {
-	cmd := exec.Command("bash", "-c", "./start.sh")
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	time.Sleep(150)
-}
-
-func stopAndRemoveDockerContainers() {
-	cmd := exec.Command("bash", "-c", "docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && sudo rm -rf compose")
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-	}
-	time.Sleep(30)
-}
-
-// case default timeout of ipfs-cluster shard retrieve is 3 minutes, this main function need 11(file number)*3 minutes at most.
-func TestAllAndFaultToler() {
-	begin := time.Now()
-	log.Infof("start docker compose")
-	err := StartDocker()
-	if err != nil {
-		log.Error(err)
-	}
-	c, err := client.NewClient()
-	if err != nil {
-		log.Error(err)
-	}
-	sth := utils.NewFileHelper()
-	tree := sth.GetTreeMultiReader()
-	err = ECAddFaultTolerantAndRetrive(c, tree)
-	if err != nil {
-		log.Error(err)
-	} else {
-		log.Infof("success to add and retrieve directory %s %d bit", tree.Name, tree.Size)
-	}
-
-	// create 1KB to 1GB files then pin and retrieve them when some nodes down
-	fs := sth.GetRandFileMultiReader()
-	for i := 0; i < len(fs); i++ {
-		err := ECAddFaultTolerantAndRetrive(c, fs[i])
-		if err != nil {
-			log.Error(err)
-			continue
-		}
-		log.Infof("success to add and retrieve ramdon file %s %d bit", fs[i].Name, fs[i].Size)
-	}
-	log.Infof("total time: %v", time.Since(begin))
-	sth.Clean()
-}
-
-func AddFaultTolerantAndRetrive(c client.Client, f utils.ECFile) error {
-	ci, err := AddECFile(f)
+func AddFaultTolerantAndRetrieve(add func(f utils.ECFile) (api.Cid, error), c *client.Client, f utils.ECFile) error {
+	ci, err := add(f)
 	if err != nil {
 		return err
 	}
-	nodes, err := CloseContainers()
+	nodes, err := checkStatusAndStop()
 	if err != nil {
 		return err
 	}
 	defer func() {
-		err = ReStartContainers(nodes)
-		if err != nil {
-			log.Error(err)
-		}
-		_, err = c.RepoGC(context.Background(), false)
-		if err != nil {
-			log.Error(err)
-		}
-	}()
-	start := time.Now()
-
-	err = c.ECGet(context.Background(), ci, utils.RetrieveDir)
-	if err != nil {
-		return fmt.Errorf("%s ERROR:%s", ci, err)
-	}
-	fmt.Printf("retrieve %s %d use %v\n", ci, f.Size, time.Since(start))
-	err = utils.Diff(path.Join(utils.SourceDir, f.Name), path.Join(utils.RetrieveDir, ci.String()))
-	if err != nil {
-		return err
-	}
-	// log.Infof("%s successfully AddFaultTolerantAndRetrive", f.Name())
-	return nil
-}
-
-func ECAddFaultTolerantAndRetrive(c client.Client, f utils.ECFile) error {
-	ci, err := AddECFile(f)
-	if err != nil {
-		return err
-	}
-	nodes, err := CloseContainers()
-	if err != nil {
-		return err
-	}
-	defer func() {
-		err = ReStartContainers(nodes)
+		err = restartNodes(nodes)
 		if err != nil {
 			log.Error(err)
 		}
@@ -155,173 +53,16 @@ func ECAddFaultTolerantAndRetrive(c client.Client, f utils.ECFile) error {
 	}()
 	err = c.ECGet(context.Background(), ci, utils.RetrieveDir)
 	if err != nil {
-		return fmt.Errorf("%s ERROR:%s", ci, err)
+		return err
 	}
 	err = utils.Diff(path.Join(utils.SourceDir, f.Name), path.Join(utils.RetrieveDir, ci.String()))
 	if err != nil {
 		return err
 	}
-	// log.Infof("%s successfully ECAddFaultTolerantAndRetrive", f.Name())
 	return nil
 }
 
-func StartDocker() error {
-	cmd := exec.Command("/bin/zsh", "start.sh")
-	err := cmd.Run()
-	time.Sleep(10 * time.Second) // wait for cluster peers set up
-	return err
-}
-
-func AddFile(f utils.ECFile) (api.Cid, error) {
-	defer f.Closer.Close()
-	c, err := client.NewClient()
-	if err != nil {
-		return api.CidUndef, err
-	}
-	ctx := context.Background()
-	params := client.DefaultAddParams
-	params.Name = f.Name
-	// st := time.Now()
-	pin, err := c.Add(ctx, path.Join(f.Base, f.Name), params)
-	if err != nil {
-		return api.CidUndef, err
-	}
-	// fmt.Printf("add %s(%v), size:%v, use %v\n", pin.Name, pin.Cid, f.Size(), time.Since(st))
-	return pin.Cid, nil
-}
-
-func AddECFile(f utils.ECFile) (api.Cid, error) {
-	defer f.Closer.Close()
-	c, err := client.NewClient()
-	if err != nil {
-		return api.CidUndef, err
-	}
-	ctx := context.Background()
-	params := client.ECAddParams
-	params.Name = f.Name
-	st := time.Now()
-	pin, err := c.Add(ctx, path.Join(f.Base, f.Name), params)
-	if err != nil {
-		return api.CidUndef, err
-	}
-	fmt.Printf("add %s(%v), size:%v, use %v\n", pin.Name, pin.Cid, f.Size, time.Since(st))
-	return pin.Cid, nil
-}
-
-func CloseContainers() ([]int, error) {
-	cli, err := dockercli.NewClientWithOpts(dockercli.FromEnv, dockercli.WithAPIVersionNegotiation())
-	defer cli.Close()
-	if err != nil {
-		return nil, err
-	}
-	// cluster[0~5]
-	x := rand.Intn(4) + 1
-	nodes := []int{x, x + 1}
-	for _, node := range nodes {
-		cluster := "cluster" + fmt.Sprintf("%d", node)
-		if err := cli.ContainerStop(context.Background(), cluster, container.StopOptions{}); err != nil {
-			return nil, err
-		}
-		ipfs := "ipfs" + fmt.Sprintf("%d", node)
-		if err := cli.ContainerStop(context.Background(), ipfs, container.StopOptions{}); err != nil {
-			return nil, err
-		}
-	}
-	time.Sleep(10 * time.Second) // metric timeout is 30s, after 30s, peer id could remove from the cluster
-	log.Infof("close %v containers", nodes)
-	return nodes, nil
-}
-
-func ReStartContainers(nodes []int) error {
-	cli, err := dockercli.NewClientWithOpts(dockercli.FromEnv, dockercli.WithAPIVersionNegotiation())
-	defer cli.Close()
-	if err != nil {
-		return err
-	}
-	for _, node := range nodes {
-		cluster := "cluster" + fmt.Sprintf("%d", node)
-		if err := cli.ContainerStart(context.Background(), cluster, container.StartOptions{}); err != nil {
-			return err
-		}
-		ipfs := "ipfs" + fmt.Sprintf("%d", node)
-		if err := cli.ContainerStart(context.Background(), ipfs, container.StartOptions{}); err != nil {
-			return err
-		}
-	}
-	time.Sleep(10 * time.Second)
-	log.Infof("start %v containers", nodes)
-	return nil
-}
-
-func TestAddECFile_DiffSize() {
-	// start()
-	// defer func() {
-	// 	stopAndRemoveDockerContainers()
-	// }()
-	sth := utils.NewFileHelper()
-	files := sth.GetRandFileMultiReader()
-	// defer sth.Clean()
-	for _, f := range files {
-		_, err := AddECFile(f)
-		if err != nil {
-			log.Error(err)
-		}
-	}
-}
-
-func TestAddECFile_LargeSameSize() {
-	start()
-	defer func() {
-		stopAndRemoveDockerContainers()
-	}()
-	sth := utils.NewFileHelper()
-	files := sth.Get512MBRandFileMultiReader()
-	defer sth.Clean()
-	for _, f := range files {
-		_, err := AddECFile(f)
-		if err != nil {
-			log.Error(err)
-		}
-	}
-}
-
-func TestAddFile_DiffSize() {
-	// start()
-	// defer func() {
-	// 	stopAndRemoveDockerContainers()
-	// }()
-	sth := utils.NewFileHelper()
-	files := sth.GetRandFileMultiReader()
-	// defer sth.Clean()
-	for _, f := range files {
-		_, err := AddFile(f)
-		if err != nil {
-			log.Error(err)
-		}
-	}
-}
-
-func TestAddFile_LargeSameSize() {
-	start()
-	defer func() {
-		stopAndRemoveDockerContainers()
-	}()
-	sth := utils.NewFileHelper()
-	files := sth.Get512MBRandFileMultiReader()
-	defer sth.Clean()
-	for _, f := range files {
-		_, err := AddFile(f)
-		if err != nil {
-			log.Error(err)
-		}
-	}
-}
-
-func TestGetFile_DiffSize() {
-	start()
-	defer func() {
-		stopAndRemoveDockerContainers()
-	}()
+func TestGetFileDiffSize() {
 	sth := utils.NewFileHelper()
 	files := sth.GetRandFileMultiReader()
 	defer sth.Clean()
@@ -330,18 +71,14 @@ func TestGetFile_DiffSize() {
 		log.Error(err)
 	}
 	for _, f := range files {
-		err = AddFaultTolerantAndRetrive(c, f)
+		err = AddFaultTolerantAndRetrieve(c.AddFile, c, f)
 		if err != nil {
 			log.Error(err)
 		}
 	}
 }
 
-func TestGetFile_LargeSameSize() {
-	start()
-	defer func() {
-		stopAndRemoveDockerContainers()
-	}()
+func TestGetFileLargeSameSize() {
 	sth := utils.NewFileHelper()
 	files := sth.Get512MBRandFileMultiReader()
 	defer sth.Clean()
@@ -350,18 +87,14 @@ func TestGetFile_LargeSameSize() {
 		log.Error(err)
 	}
 	for _, f := range files {
-		err = AddFaultTolerantAndRetrive(c, f)
+		err = AddFaultTolerantAndRetrieve(c.AddFile, c, f)
 		if err != nil {
 			log.Error(err)
 		}
 	}
 }
 
-func TestECGetFile_DiffSize() {
-	start()
-	defer func() {
-		stopAndRemoveDockerContainers()
-	}()
+func TestECGetFileDiffSize() {
 	sth := utils.NewFileHelper()
 	files := sth.GetRandFileMultiReader()
 	defer sth.Clean()
@@ -370,18 +103,14 @@ func TestECGetFile_DiffSize() {
 		log.Error(err)
 	}
 	for _, f := range files {
-		err = ECAddFaultTolerantAndRetrive(c, f)
+		err = AddFaultTolerantAndRetrieve(c.AddECFile, c, f)
 		if err != nil {
 			log.Error(err)
 		}
 	}
 }
 
-func TestECGetFile_LargeSameSize() {
-	start()
-	defer func() {
-		stopAndRemoveDockerContainers()
-	}()
+func TestECGetFileLargeSameSize() {
 	sth := utils.NewFileHelper()
 	files := sth.Get512MBRandFileMultiReader()
 	defer sth.Clean()
@@ -390,7 +119,7 @@ func TestECGetFile_LargeSameSize() {
 		log.Error(err)
 	}
 	for _, f := range files {
-		err = ECAddFaultTolerantAndRetrive(c, f)
+		err = AddFaultTolerantAndRetrieve(c.AddECFile, c, f)
 		if err != nil {
 			log.Error(err)
 		}
@@ -398,10 +127,6 @@ func TestECGetFile_LargeSameSize() {
 }
 
 func TestECRecovery() {
-	// start()
-	// defer func() {
-	// 	stopAndRemoveDockerContainers()
-	// }()
 	sth := utils.NewFileHelper()
 	files := sth.GetRandFileMultiReader()
 	defer sth.Clean()
@@ -411,13 +136,13 @@ func TestECRecovery() {
 	}
 	cis := make([]api.Cid, 0)
 	for _, f := range files {
-		ci, err := AddECFile(f)
+		ci, err := c.AddECFile(f)
 		cis = append(cis, ci)
 		if err != nil {
 			log.Error(err)
 		}
 	}
-	_, err = CloseContainers()
+	err = stopNodes([]string{})
 	if err != nil {
 		log.Error(err)
 	}
@@ -431,4 +156,37 @@ func TestECRecovery() {
 	for r := range out {
 		fmt.Printf("%v\n", r)
 	}
+}
+
+func checkStatusAndStop() ([]string, error) {
+	cmd := exec.Command("bash", "-c", "dctl status --filter pinned --sort name")
+	output, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	nodes := []string{string(output)}
+	fmt.Printf("status output: %s\n", output)
+	err = stopNodes(nodes)
+	if err != nil {
+		return nil, err
+	}
+	return nodes, nil
+}
+
+func stopNodes(nodes []string) error {
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("for node in %s; do ansible $node -m ansible.builtin.systemd -a \"name=ipfs state=stopped\" -b; ansible $node -m ansible.builtin.systemd -a \"name=ipfs-cluster state=stopped\" -b; done", nodes))
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func restartNodes(nodes []string) error {
+	cmd := exec.Command("bash", "-c", fmt.Sprintf("for node in %s; do ansible $node -m ansible.builtin.systemd -a \"name=ipfs state=started\" -b; ansible $node -m ansible.builtin.systemd -a \"name=ipfs-cluster state=started\" -b; done", nodes))
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
 }
